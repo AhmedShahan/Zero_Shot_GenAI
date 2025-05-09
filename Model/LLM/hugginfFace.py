3a42b9c13a776251ae3b82c7dc736b7ad84c004d
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-09 15:40:55 +0600

    higgingface model responces

--- Model/LLM/hugginfFace.py
+++ Model/LLM/hugginfFace.py
@@ -1,15 +1,14 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+import os
 from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint
 
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation"
+    repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
 )
 
-model = ChatHuggingFace(llm=llm)
+response = llm.invoke("What is the capital of Bangladesh?")
 
-result = model.invoke("What is the capital of India")
-
-print(result.content)
+print(response)
