effc5502e16453eabca4ad1fe2e89ea8cf1fd157
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-10 12:39:21 +0600

    huggingface only mistral works others are not

--- Model/ChatModel/huggingFace_Chatmodel.py
+++ Model/ChatModel/huggingFace_Chatmodel.py
@@ -1,16 +1,38 @@
-import os
+# from dotenv import load_dotenv
+# import os
+# from langchain_huggingface import HuggingFaceEndpoint
+
+# load_dotenv()
+
+# # Initialize the HuggingFace model
+# llm = HuggingFaceEndpoint(
+#     repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
+#     huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+# )
+
+# # Get response
+# response = llm.invoke("Tell me a joke about a cat.")
+
+# # Print response
+# print(response)
+
+
+
+
 from dotenv import load_dotenv
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+import os
+from langchain_huggingface import HuggingFaceEndpoint
 
 load_dotenv()
 
+# Initialize the HuggingFace model
 llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
-    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+    repo_id="togethercomputer/llama-2-7b-chat",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
 )
 
-chat_model = ChatHuggingFace(llm=llm)
+# Get response
+response = llm.invoke("Tell me a joke about a cat.")
 
-response = chat_model.invoke("What is the capital of Bangladesh?")
-
-print(response.content)
+# Print response
+print(response)
