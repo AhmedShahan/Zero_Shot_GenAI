6385ba4198abff9b2029962895ae5bf0acc022b6
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-11 18:16:28 +0600

    cosene scores calculate

--- QuerySimilarity.py
+++ QuerySimilarity.py
@@ -4,11 +4,11 @@
 ## Let say We have some documents of 5 Different Person in a document list
 
 documents=[
-    "Shakib Al Hasan is a world-class all-rounder, excelling in left-arm spin and aggressive batting, consistently ranked among the top all-rounders globally. He holds records like Bangladesh’s highest ODI wicket-taker."
-    "Tamim Iqbal, Bangladesh’s top run-scorer, is known for his elegant left-handed batting. He scored the nation’s first Test double century."
-    "Mushfiqur Rahim, a gritty wicketkeeper-batsman, has been crucial in many victories. He was the first Bangladeshi to score a Test double century."
-    "Mahmudullah Riyad, a versatile middle-order batsman and off-spinner, shines in clutch T20 and ODI moments. He has captained Bangladesh with composure."
-    "Mustafizur Rahman, a left-arm pacer, is famed for his deceptive cutters, dominating limited-overs cricket. His unique style earned IPL stardom."
+    "Shakib Al Hasan is a world-class all-rounder, excelling in left-arm spin and aggressive batting, consistently ranked among the top all-rounders globally. He holds records like Bangladesh’s highest ODI wicket-taker.",
+    "Tamim Iqbal, Bangladesh’s top run-scorer, is known for his elegant left-handed batting. He scored the nation’s first Test double century.",
+    "Mushfiqur Rahim, a gritty wicketkeeper-batsman, has been crucial in many victories. He was the first Bangladeshi to score a Test double century.",
+    "Mahmudullah Riyad, a versatile middle-order batsman and off-spinner, shines in clutch T20 and ODI moments. He has captained Bangladesh with composure.",
+    "Mustafizur Rahman, a left-arm pacer, is famed for his deceptive cutters, dominating limited-overs cricket. His unique style earned IPL stardom.",
 ]
 
 
@@ -23,9 +23,27 @@ Query="Tell Me about Sakib Al Hasan"
 
 QueryEmbed=embedding.embed_query(text=Query)
 
-print("Document Embedding")
-print(DocumentEmbed)
+# print("Document Embedding")
+# print(DocumentEmbed)
 
 
-print("Query Embedding")
-print(QueryEmbed)
+# print("Query Embedding")
+# print(QueryEmbed)
+
+##### Checking the Cosine Similarity
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+### cosine similarity(x,y)
+# x= query embedding in 2D Array, already Embedding korar somoy 1D list e ache
+# y= Document Embedding, Already 2D array tei ache. 
+
+score=cosine_similarity([QueryEmbed],DocumentEmbed)
+print(score)
+
+'''
+Returns 2D So make it 1D
+'''
+score=cosine_similarity([QueryEmbed],DocumentEmbed)
+print(score[0])
