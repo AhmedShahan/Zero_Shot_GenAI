5f3859503eafa0e8198a36867aab39d9cd2b2a33
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-12 13:07:12 +0600

    all sentence embedding link huggingface and change one

--- Model/Embedding_Model/3_Embedding_OpenSource_sentence_transformer.py
+++ Model/Embedding_Model/3_Embedding_OpenSource_sentence_transformer.py
@@ -5,7 +5,8 @@ load_dotenv()  # Load the API key
 
 
 
-embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/average_word_embeddings_levy_dependency")
+# All sentence tranformer models are available in HuggingFace Embeddings: https://huggingface.co/sentence-transformers 
 
 text="Dhaka is the capital of Bangladesh. It is the largest city in the country and serves as its political, economic, and cultural center."
 
