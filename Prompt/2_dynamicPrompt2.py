343cd0345de2ef2f40d34d01333558a9bffb08c3
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-14 16:05:11 +0600

    prompt templet another method easy

---
+++ Prompt/2_dynamicPrompt2.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAI, ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+
+
+
+
+st.title("Basic Summarize Chatbot Using Open Source LLM")
+st.write("This is a simple chatbot that uses Open Source LLMs to summarize text.")
+
+
+
+paper_title=st.selectbox("Select a Paper", ["Attention is All You Need", 
+                                "BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding", 
+                                "Language Models are Unsupervised Multitask Learners",
+                                "Scaling Laws for Neural Language Models",
+                                "Training Language Models to Follow Instructions with Human Feedback",
+                                "T5: Exploring the Limits of Transfer Learning with a Unified Text-to-Text Transformer",
+                                "XLNet: Generalized Autoregressive Pretraining for Language Understanding",
+                                "RoBERTa: A Robustly Optimized BERT Pretraining Approach",
+                                "ALBERT: A Lite BERT for Self-supervised Learning of Language Representations",
+                                "DistilBERT: A Distilled Version of BERT: Smaller, Faster, Cheaper, and Lighter",
+                                "ERNIE: Enhanced Representation through kNowledge Integration",
+                                ])
+
+explanation_style= st.selectbox("Select Explanation Style", ["Beginer-Friendly", 
+                                            "Code Oriented", 
+                                            "Mathematical", 
+                                            "Technical", 
+                                            "Expert Level"])
+explanation_length= st.selectbox("Select Length of Explanation", ["Short", 
+                                                "Medium", 
+                                                "Long"])
+
+
+Research_prompt="""
+Please summarize the research paper titled '{paper_title}' with the following specification: 
+Explain Style: {explanation_style}
+Explanation Lenght: {explanation_length}
+1. Mathematical Details: 
+    - Include relevent Mathematical equations if present in paper. 
+    - Explain the mathematical concept using simple, intutive code snippet where applicable. 
+
+2. Technical Details:
+    - Include relevent technical details if present in paper. 
+    - Explain the technical concept using simple, intutive code snippet where applicable.
+
+3. Code Oriented:
+    - Include relevent code snippets if present in paper. 
+    - Explain the code using simple, intutive code snippet where applicable.
+
+4. Analogical: 
+    - Use relevant analogies to simplify complex concepts or ideas.    
+If certain information is not in the paper, respond with "Information not available in the paper" instead of guessing or making assumptions.
+Ensure that the summary is clear, concise, and easy to understand for someone who may not be familiar with the topic.
+"""
+
+
+
+
+templet=ChatPromptTemplate.from_template(Research_prompt)
+
+## Fill the placeholder with the user input
+final_prompt=templet.invoke({
+    "paper_title":paper_title,
+    "explanation_style": explanation_style,
+    "explanation_length": explanation_length
+})
+
+model= ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.5)
+
+if st.button("Summarize"):
+    st.write("Generating summary...")
+        # First, format the prompt with the user's selections
+    result= model.invoke(final_prompt)
+    st.write("Google LLM Response:", result.content)
