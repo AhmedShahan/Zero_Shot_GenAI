3ed9ac22c1586d7fb1f22ef5e8aacd6653139499
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-14 16:11:38 +0600

    dynamic message easy way

--- Message/8_dynamic_message.py
+++ Message/8_dynamic_message.py
@@ -2,12 +2,15 @@ from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
 
 
-chatTemplet=ChatPromptTemplate([
+Message=[
     ('system', "You are a helpful {domain} assistant."),
     ('human', "What is the {level} level of {input}?"),
-])
+]
 
-prompt=chatTemplet.invoke({"domain":"AI", "input":"Python", "level":"easy"})
+prompt= ChatPromptTemplate.from_messages(Message)
+
+
+response=prompt.invoke({"domain":"AI", "input":"Python", "level":"easy"})
 
 print(prompt)
 
