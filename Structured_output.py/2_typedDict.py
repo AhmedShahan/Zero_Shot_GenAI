3c6b1e9fdf0bc68ecd8ef9dda284d7cbfefa111a
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-19 14:31:43 +0600

    only single value can be pass

--- Structured_output.py/2_typedDict.py
+++ Structured_output.py/2_typedDict.py
@@ -7,7 +7,7 @@ class person(TypedDict):
 new_person1=person(name="Shahan",cgpa=3.5)
 new_person2=person(name="Ahmed",cgpa=3.7)
 
-
+new_person4=person(name="Sinthiya")
 new_person3= person(name="Shahan", cgpa="3.9")
-print(new_person3)
+print(new_person4)
 ### There is no error after passing cgpa as string
