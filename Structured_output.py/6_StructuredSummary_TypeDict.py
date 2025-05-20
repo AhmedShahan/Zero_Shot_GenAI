4084d7afce062b51186485bb60a481005c5f0acc
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-20 11:23:35 +0600

    structure define and structured model create

--- Structured_output.py/6_StructuredSummary_TypeDict.py
+++ Structured_output.py/6_StructuredSummary_TypeDict.py
@@ -1,6 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated
 load_dotenv()
 
 
@@ -10,7 +10,20 @@ model=ChatGoogleGenerativeAI(
 )
 
 prompt="write down the summary of Attendtion All you Need Paper"
-
 summary = model.invoke(prompt)
 
-print(summary.content)
+class StructuredSummary(TypedDict):
+    title: Annotated[str, "Write down the title of the paper"]
+    authors: Annotated[list[str], "Write down the authors' names in a list"]
+    key_term: Annotated[str, "Write down all the key terms mentioned in the paper"]
+    keywords: Annotated[list[str], "Write down the keywords of the paper in a list"]
+    abstract: Annotated[str, "Write a short summary or abstract of the paper"]
+    problem_statement: Annotated[str, "Describe the problem or research gap the paper addresses"]
+    methodology: Annotated[str, "Explain the methods, models, or frameworks used in the paper"]
+    results: Annotated[str, "Summarize the main results or findings of the paper"]
+    conclusion: Annotated[str, "Write down the conclusion or takeaway from the paper"]
+    future_work: Annotated[str, "Mention any suggested future directions or work"]
+    citation: Annotated[str, "Provide the formatted citation (IEEE/APA/etc.) of the paper"]
+    paper_link: Annotated[str, "Include the link or DOI of the paper"]
+
+structured_model=model.with_structured_output(StructuredSummary)
