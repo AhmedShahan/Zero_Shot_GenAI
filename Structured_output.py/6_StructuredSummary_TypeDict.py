56152bdf325ba1600b52b95e940a1baede0977d3
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-20 11:33:57 +0600

    print method is chaned

--- Structured_output.py/6_StructuredSummary_TypeDict.py
+++ Structured_output.py/6_StructuredSummary_TypeDict.py
@@ -16,7 +16,6 @@ summary_content=summary.content
 class StructuredSummary(TypedDict):
     title: Annotated[str, "Write down the title of the paper"]
     authors: Annotated[list[str], "Write down the authors' names in a list"]
-    key_term: Annotated[list[str], "Write down all the key terms mentioned in the paper"]
     keywords: Annotated[list[str], "Write down the keywords of the paper in a list"]
     abstract: Annotated[str, "Write a short summary or abstract of the paper"]
     problem_statement: Annotated[str, "Describe the problem or research gap the paper addresses"]
@@ -33,15 +32,15 @@ structured_model=model.with_structured_output(StructuredSummary)
 structured_response= structured_model.invoke(summary_content)
 
 # Print all elements
-print("TTtitle:", structured_response["title"])
-# print("Authors:", structured_response["authors"])
-print("Key Term:", structured_response["key_term"])
-print("Keywords:", structured_response["keywords"])
-print("Abstract:", structured_response["abstract"])
-print("Problem Statement:", structured_response["problem_statement"])
-print("Methodology:", structured_response["methodology"])
-print("Results:", structured_response["results"])
-print("Conclusion:", structured_response["conclusion"])
-print("Future Work:", structured_response["future_work"])
-print("Citation:", structured_response["citation"])
-print("Paper Link:", structured_response["paper_link"])
+print("Title:", structured_response.get("title", "N/A"))
+print("Authors:", structured_response.get("authors", []))
+print("Key Term:", structured_response.get("key_term", "N/A"))
+print("Keywords:", structured_response.get("keywords", []))
+print("Abstract:", structured_response.get("abstract", "N/A"))
+print("Problem Statement:", structured_response.get("problem_statement", "N/A"))
+print("Methodology:", structured_response.get("methodology", "N/A"))
+print("Results:", structured_response.get("results", "N/A"))
+print("Conclusion:", structured_response.get("conclusion", "N/A"))
+print("Future Work:", structured_response.get("future_work", "N/A"))
+print("Citation:", structured_response.get("citation", "N/A"))
+print("Paper Link:", structured_response.get("paper_link", "N/A"))
