da7f3a196fbcc1aa215f89d6dc04b6b50320f043
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-21 10:12:03 +0600

    how pydantic works in validation

--- Structured_output.py/7_pydantic_basic.py
+++ Structured_output.py/7_pydantic_basic.py
@@ -12,8 +12,25 @@ class student(BaseModel):
 
 
 new_student = student(name="shahan", age=20, grade=3.5, is_enrolled=True)
+# print(new_student)
+# print(new_student.name)
+# print(new_student.age)
+# print(new_student.grade)
+# print(new_student.is_enrolled)
+
+### This code is working well but if we pass wrong typed data it will throw an error
+new_student = student(name="shahan", age="twenty", grade=3.5, is_enrolled=True)
 print(new_student)
-print(new_student.name)
-print(new_student.age)
-print(new_student.grade)
-print(new_student.is_enrolled)
+
+'''
+Traceback (most recent call last):
+  File "/home/shahanahmed/Zero_Shot_GenAI/Structured_output.py/7_pydantic_basic.py", line 22, in <module>
+    new_student = student(name="shahan", age="twenty", grade=3.5, is_enrolled=True)
+  File "/home/shahanahmed/Zero_Shot_GenAI/myvenv/lib/python3.10/site-packages/pydantic/main.py", line 253, in __init__
+    validated_self = self.__pydantic_validator__.validate_python(data, self_instance=self)
+pydantic_core._pydantic_core.ValidationError: 1 validation error for student
+age
+  Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='twenty', input_type=str]
+    For further information visit https://errors.pydantic.dev/2.11/v/int_parsing
+
+'''
