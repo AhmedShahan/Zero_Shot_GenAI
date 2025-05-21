a5429daead93ac7d8579065a9ed77a5187e98333
AhmedShahan <shahan.ahmed001@gmail.com>
2025-05-21 15:47:21 +0600

    optional but required default explanation

--- Structured_output.py/9_pydantic_optional.py
+++ Structured_output.py/9_pydantic_optional.py
@@ -1,27 +1,22 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 class student(BaseModel):
-    name: str = "Ahmed"
-    age: int = 20
-    grade: float = 3.5
-    is_enrolled: Optional[bool] = None
-    email: Optional[EmailStr] = None
-
-new_student = student()
-print(new_student)
-print(new_student.name)
-print(new_student.age)
-print(new_student.grade)
-print(new_student.is_enrolled)
-print(new_student.__dict__)   ## It will return the full dictionary
-print(type(new_student))  ## It will return the type of the object
-
-
-
-new_student1= student(name="Ali", age=25, grade=3.8, is_enrolled=True)
-print(new_student1)
-
+    name: str
+    age: int
+    grade: float
+    is_enrolled: Optional[bool]
+    email: Optional[EmailStr]
 
 ## pip install pydantic[email]
-new_student3= student(name="Shahan", age=25, grade=3.8, is_enrolled=True, email="abc")
+new_student3= student(name="Shahan", age=25, grade=3.8,email="abc@gmail.com")
 print(new_student3)
+
+
+'''
+Although is_enrolled is optional, it is not required to be present in the input data. Because 
+In Pydantic v2, using Optional[bool] without a default value means the field is still required unless explicitly marked as optional with a default like None.
+That why we have to set it as None
+
+
+In a Nutshell, if we provide optional field, then it will requiredd the default value as None or any other value
+'''
