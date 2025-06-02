e4dd74421551ca1ff18ce12052cb426f7176a27b
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-02 10:29:27 +0600

    runnablepassthrough content generation and idea

---
+++  Runnables/7_Runnable_lembda.py
@@ -0,0 +1,38 @@
+'''
+Main Target is: 
+Generate Content with totasl number of word. 
+Content Generate : LLM
+Word Count: python function
+'''
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_cohere import ChatCohere
+from dotenv import load_dotenv
+
+# Load environment variables (e.g., Google API key)
+load_dotenv()
+
+MessageContent = [
+    ('system', 'You are an expert AI helper for Content Generation. Please provide an Detailed Information for the given topic.'),
+    ('human', 'Please provide a Detailed Information on the topic: {topic}')
+]
+
+promptContent=ChatPromptTemplate.from_messages(MessageContent)
+parser = StrOutputParser()
+
+# Initialize the model
+model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=1.5)
+chain=promptContent | model | parser
+
+topic="Artificial Intelligent"
+
+result= chain.invoke({
+    "topic":topic
+})
+
+print(result)
