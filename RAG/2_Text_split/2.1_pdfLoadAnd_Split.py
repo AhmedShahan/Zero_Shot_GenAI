7cab220a664c2bc19b67228ffdbe72437ad3c7e5
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-04 10:37:07 +0600

    every chunk pagecontent and metadata print

--- RAG/2_Text_split/2.1_pdfLoadAnd_Split.py
+++ RAG/2_Text_split/2.1_pdfLoadAnd_Split.py
@@ -19,5 +19,7 @@ splited_text=spliter.split_documents(docs)
 print("Total Chunk: ",len(splited_text))
 for i in range (len(splited_text)):
     print("Chunk: ",i)
-    print(splited_text[i])
+    text=splited_text[i]
+    print("Chunk Content: ",text.page_content)
+    print("Chunk Metadata: ",text.metadata)
     print("*"*50)
