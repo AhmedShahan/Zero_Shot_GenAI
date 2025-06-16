ffaac59b94583b67545225b2d6012bcc8a46e03d
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-16 17:02:03 +0600

    from load to vector database

--- RAG/3_vector_store/chromaDB/5_loadToStore.py
+++ RAG/3_vector_store/chromaDB/5_loadToStore.py
@@ -2,6 +2,10 @@
 
 from langchain.document_loaders import PyPDFLoader
 
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+
 loader=PyPDFLoader("/home/shahanahmed/Zero_Shot_GenAI/RAG/documents/pdfs/Bangladesh.pdf")
 
 docs=loader.load()
@@ -26,6 +30,17 @@ chunks=splitter.split_documents(docs)
 # #     print(chunk.page_content)
 
 
-for i, chunk in enumerate(chunks):
-    print(f"---- Chunk {i+1} -----")
-    print(chunk.page_content)
+# for i, chunk in enumerate(chunks):
+#     print(f"---- Chunk {i+1} -----")
+#     print(chunk.page_content)
+
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/average_word_embeddings_levy_dependency")
+vector_store=Chroma(
+    embedding_function=embedding, 
+    persist_directory="/home/shahanahmed/Zero_Shot_GenAI/RAG/3_vector_store/chromaDB",
+    collection_name="sample"
+)
+
+vector_store.add_documents(chunks)
+storedElement=vector_store.get(include=['embeddings','documents','metadatas'])
+print(storedElement)
