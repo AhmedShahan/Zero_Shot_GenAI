d8963c909aea82eb9b3ed47ecde3dcf8921f49d0
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-17 10:32:59 +0600

    minor changes and metadat in wikipedia search

--- RAG/4_retriver/1_wikipedia_search.py
+++ RAG/4_retriver/1_wikipedia_search.py
@@ -6,10 +6,16 @@ from langchain_community.retrievers import WikipediaRetriever
 
 retriever=WikipediaRetriever(
     top_k_results=10,
+    load_all_available_meta=True
 )
 
 query="Who is the CEO of Meta?"
 
 docs=retriever.invoke(query)
 
-print(docs)
+# print(docs)
+for doc in docs:
+    print("Document: ",doc.page_content)
+    print("Metadata",doc.metadata)
+    print("*"*50)
+
