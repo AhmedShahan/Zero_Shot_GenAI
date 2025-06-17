f1880701bc792bdb596ba980e66ab8078412ff70
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-17 10:37:09 +0600

    tavely search

---
+++ RAG/4_retriver/2_tavlySearch.py
@@ -0,0 +1,21 @@
+'''
+pip install tavily-python
+'''
+
+from langchain_community.retrievers import TavilySearchAPIRetriever
+from dotenv import load_dotenv
+load_dotenv()
+retriever=TavilySearchAPIRetriever(
+    k=10
+)
+
+
+query="Who is the CEO of Meta?"
+
+docs=retriever.invoke(query)
+
+# print(docs)
+for doc in docs:
+    print("Document: ",doc.page_content)
+    print("Metadata",doc.metadata)
+    print("*"*50)
