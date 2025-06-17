5efc1e06acb2af3d4174bda296d71572c244dbe3
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-17 10:48:24 +0600

    pubmed is working better then arxive

---
+++ RAG/4_retriver/4_pubMedSearch.py
@@ -0,0 +1,19 @@
+'''
+pip install xmltodict
+'''
+
+from langchain_community.retrievers import PubMedRetriever
+
+retriever = PubMedRetriever(
+    top_k_results=10
+)
+
+query="Authors: Shafin Rahman"
+
+docs=retriever.invoke(query)
+
+# print(docs)
+for doc in docs:
+    print("Document: ",doc.page_content)
+    print("Metadata",doc.metadata)
+    print("*"*50)
