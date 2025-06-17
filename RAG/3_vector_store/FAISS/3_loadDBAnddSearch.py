be12249aad5fdaca898b024d49aa0c4eb08c0199
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-17 14:53:17 +0600

    load and search query

---
+++ RAG/3_vector_store/FAISS/3_loadDBAnddSearch.py
@@ -0,0 +1,27 @@
+from langchain.vectorstores import FAISS
+from langchain.schema import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+# Step 2: Initialize embeddings
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/average_word_embeddings_levy_dependency")
+
+
+
+# Optional: Load the index later like this
+# Load the FAISS index from disk safely
+loaded_vectorstore = FAISS.load_local(
+    "/home/shahanahmed/Zero_Shot_GenAI/RAG/3_vector_store/FAISS/faiss_db",
+    embeddings=embedding,
+    allow_dangerous_deserialization=True  # Add this line
+)
+
+
+# Search for similar documents
+query = "Who is a powerful middle order batsman?"
+results = loaded_vectorstore.similarity_search(query, k=2)
+
+for res in results:
+    print("Matched Content:", res.page_content)
+    print("Team:", res.metadata['team'])
+    print("-" * 60)
