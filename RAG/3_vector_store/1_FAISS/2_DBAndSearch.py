7d53909ba7d8c40bd2cf0f0223a14bd986420a13
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-19 16:21:43 +0600

    run time query FAISS

--- RAG/3_vector_store/1_FAISS/2_DBAndSearch.py
+++ RAG/3_vector_store/1_FAISS/2_DBAndSearch.py
@@ -34,11 +34,8 @@ docs = [doc1, doc2, doc3, doc4, doc5]
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/average_word_embeddings_levy_dependency")
 
 # Step 3: Create FAISS vector store
-vectorstore = FAISS(
-    embedding_function=embedding
-)
+vectorstore = FAISS.from_documents(docs, embedding)
 
-vectorstore.add_documents(docs)
 
 
 # Search for similar documents
