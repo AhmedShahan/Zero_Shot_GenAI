98dbaf4b8b453e881eb7bef0cf0ad11893a77b11
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-20 21:02:25 +0600

    prompt with query

--- Prompt/5_batch_prompt_inChain.py
+++ Prompt/5_batch_prompt_inChain.py
@@ -1,12 +1,25 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 load_dotenv()
 
-model=ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.5)
+model=ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.)
 
 query="What is AI"
 
-response=model.invoke(query)
+
+MessageExplain=[
+    ('system','You are an Expart System for Explanation. Please Explain the topic in breaf manner.'),
+    ('human','Please Explain the topic {topic}')
+]
+prompt=ChatPromptTemplate.from_messages(MessageExplain)
+
+chain=prompt | model
+
+
+query="Artificial Intelligence"
+response=chain.invoke({
+    "topic":query
+})
 
 print(response.content)
