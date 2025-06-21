ea6ca9c65327c5428bd6b670377d119013d1c86c
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-21 17:29:32 +0600

    minoir  changes in pubmed  retriever

--- RAG/4_retriver/websearch/4_pubMedSearch.py
+++ RAG/4_retriver/websearch/4_pubMedSearch.py
@@ -2,7 +2,7 @@
 pip install xmltodict
 '''
 
-from langchain_community.retrievers import PubMedRetriever
+from langchain.retrievers import PubMedRetriever
 
 retriever = PubMedRetriever(
     top_k_results=10
@@ -12,8 +12,8 @@ query="Authors: Shafin Rahman"
 
 docs=retriever.invoke(query)
 
-# print(docs)
-for doc in docs:
-    print("Document: ",doc.page_content)
-    print("Metadata",doc.metadata)
-    print("*"*50)
+print(docs)
+# for doc in docs:
+#     print("Document: ",doc.page_content)
+#     print("Metadata",doc.metadata)
+#     print("*"*50)
