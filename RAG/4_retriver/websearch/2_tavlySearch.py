276c9d3ff4c4d6ae572387674d2a5a3c0ea483ef
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-21 15:53:24 +0600

    tavily some parameters

--- RAG/4_retriver/websearch/2_tavlySearch.py
+++ RAG/4_retriver/websearch/2_tavlySearch.py
@@ -2,21 +2,27 @@
 pip install tavily-python
 '''
 
-from langchain_community.retrievers import TavilySearchAPIRetriever
+from langchain.retrievers import TavilySearchAPIRetriever
 from dotenv import load_dotenv
 load_dotenv()
 retriever=TavilySearchAPIRetriever(
-    k=10
+    include_generated_answer=True,
+    include_images=True,
+    k=2     ,
+    include_raw_content=True,
+    # search_depth=SearchDepth.ADVANCED,
+
+
 )
 
 
 query="Who is the CEO of Meta?"
 
-# docs=retriever.invoke(query)
-docs=retriever.get_relevant_documents(query)
-print(docs)
-# # print(docs)
-# for doc in docs:
-#     print("Document: ",doc.page_content)
-#     print("Metadata",doc.metadata)
-#     print("*"*50)
+docs=retriever.invoke(query)
+# docs=retriever.get_relevant_documents(query)
+# print(docs)
+# print(docs)
+for doc in docs:
+    print("Document: ",doc.page_content)
+    print("Metadata",doc.metadata)
+    print("*"*50)
