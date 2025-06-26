52fc68c8eead67c365728642834f0956d141d460
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-26 10:34:55 +0600

    change loader andd split

--- RAG/RAG_pdf2.py
+++ RAG/RAG_pdf2.py
@@ -1,14 +1,25 @@
 path = "/home/shahanahmed/Zero_Shot_GenAI/RAG/documents/Electronic Medical Record (EMR) System_Task_Breakdown.pdf"
 
 
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import UnstructuredPDFLoader
 
-loader=PyPDFLoader(path)
+loader=UnstructuredPDFLoader(path)
 docs = loader.load()
 total_page=len(docs)
 print(f"Loaded {total_page} pages from PDF")
 
-for i  in range  (total_page):
-    print(f"Page {i} Content")
-    print(docs[i].page_content)
+# for i  in range  (total_page):
+#     print(f"Page {i} Content")
+#     print(docs[i].page_content)
+#     print("*"*50)
+
+############## Text Split ###########
+from langchain.text_splitter import  CharacterTextSplitter
+splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100, separator=" ")
+chunks=splitter.split_documents(docs)
+total_chunks=len(chunks)
+print(f"Total chunks {total_chunks}")
+
+for  chunk  in chunks:
+    print(chunk.page_content)
     print("*"*50)
