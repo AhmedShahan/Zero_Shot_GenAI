05eb6a97aaca694fb7228eb79182940dd420923f
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-26 12:23:15 +0600

    embedding and vector store

--- RAG/RAG_pdf2.py
+++ RAG/RAG_pdf2.py
@@ -1,9 +1,9 @@
 path = "/home/shahanahmed/Downloads/Vector Store Recommendations.pdf"
 
 
-from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import PyMuPDFLoader
 
-loader=UnstructuredPDFLoader(path)
+loader=PyMuPDFLoader(path)
 docs = loader.load()
 total_page=len(docs)
 print(f"Loaded {total_page} pages from PDF")
@@ -20,6 +20,19 @@ chunks=splitter.split_documents(docs)
 total_chunks=len(chunks)
 print(f"Total chunks {total_chunks}")
 
-for  chunk  in chunks:
-    print(chunk.page_content)
-    print("*"*50)
+# for  chunk  in chunks:
+#     print(chunk.page_content)
+#     print("*"*50)
+
+############## Embedding ###########
+from langchain_huggingface import HuggingFaceEmbeddings
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+############## Vector Store ###########
+from langchain.vectorstores import Chroma
+vector_store = Chroma(embedding_function=embedding, collection_name="sample")
+
+# Add chunks to vector store
+vector_store.add_documents(chunks)
+print(f"Documents in vector store: {vector_store._collection.count()}")
