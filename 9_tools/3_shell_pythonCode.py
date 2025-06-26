66c79a7f2c664c88ef1df2c701f338dff0902ebf
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-26 17:58:09 +0600

    try to python code in shell tool

--- 9_tools/3_shell_pythonCode.py
+++ 9_tools/3_shell_pythonCode.py
@@ -3,7 +3,16 @@ from langchain_community.tools import ShellTool
 tool = ShellTool()
 
 # Python code as a shell command
-command = 'python3 -c "numbers = [5, 2, 9, 1, 7]; numbers.sort(); print(\'Sorted list:\', numbers)"'
+command = '''
+python3
+"
+a = [10, 20, 30, 40]
+numbers = [5, 2, 9, 1, 7]
+numbers.sort()
+print("Sorted list:", numbers)
+"
+'''
+
 
 # Run the command
 result = tool.run(command)
