01c68c3eedd057c46d32a61c97a9e294611e13e7
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-27 10:37:55 +0600

    custom tools multiply invoke

---
+++ 9_tools/customTools/1_MultiplyTool.py
@@ -0,0 +1,8 @@
+from langchain_core.tools import tool
+@tool
+def multiply(num1:int, num2:int)-> int:
+    """Multiply two  numbers"""
+    return num1  * num2
+
+result=multiply.invoke({"num1":5, "num2":10})
+print(result)
