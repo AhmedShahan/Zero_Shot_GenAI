ffb444750f948d807c4a51effb6847480757381f
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-28 10:23:12 +0600

    chatbot tool calling check

--- 9_tools/customTools/8_moreConditionalToolCall.py
+++ 9_tools/customTools/8_moreConditionalToolCall.py
@@ -1,6 +1,9 @@
 from langchain_core.tools import tool
 from langchain_community.tools.arxiv import ArxivQueryRun
 from langchain_community.tools import DuckDuckGoSearchResults
+from langchain_cohere import ChatCohere
+from dotenv import load_dotenv
+load_dotenv()
 @tool
 def multiply(a:int, b:int)->int:
     """Multiplication of two integer number"""
@@ -10,19 +13,19 @@ tavily_tool = ArxivQueryRun()
 duckduckgo=DuckDuckGoSearchResults()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.9)
+llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.7)
+# llm = ChatCohere(model="command-r-plus")
 
 ## tool binding
 llm_with_tool=llm.bind_tools([multiply,tavily_tool,duckduckgo])
 
-query="Conference paper on sign language"
-result=llm_with_tool.invoke(query)
-tool_call=result.tool_calls[0]
-print("Tool Calls: ",tool_call)
-
-# result=llm_with_tool.invoke(query)
-# if result.content:
-#     print("Without Tool Call Result: ",result)
-# else:
-#     result=multiply.invoke(result.tool_calls[0])
-#     print("With Multiply ToolCall Result:",result.content)
+while True:
+    query=input("enter Query: ")
+    result=llm_with_tool.invoke(query)
+    # print(result.content)
+    try:
+        tool_call=result.tool_calls[0]
+        print("Tool Calls: ",tool_call['name'])
+    except:
+        result=llm.invoke(query)
+        print(result.content)
