fb22a9fbb937452990a25015ad43bd0a9c5f5e01
AhmedShahan <shahan.ahmed001@gmail.com>
2025-09-11 17:41:27 +0600

    minor changes

--- 1_Model/ChatModel/async2.py
+++ 1_Model/ChatModel/async2.py
@@ -3,12 +3,12 @@ import time
 
 async def tea():
     print("Making Tea")
-    await asyncio.sleep(3)
+    await asyncio.sleep(5)
     print("Deliver Tea")
 
 async def coffee():
     print("Making Coffee")
-    await asyncio.sleep(5)
+    await asyncio.sleep(10)
     print("Deliver Coffee")
 
 async def main():
@@ -24,7 +24,7 @@ async def main():
     # কে আগে শেষ হবে, সেটা ডাইনামিক ভাবে ধরতে চাই
     for finished in asyncio.as_completed(tasks):
         await finished  # যতক্ষণ না একেকটা task শেষ হয়
-        print(f"One task finished at {time.time()-st:.2f} seconds")
+        print(f"Task finished at {time.time()-st:.2f} seconds")
 
     et = time.time()
     print("Total time:", et - st)
