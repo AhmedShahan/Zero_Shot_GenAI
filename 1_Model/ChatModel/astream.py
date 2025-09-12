82a1ae52b42d9410740be4c0ab896b5d26b79d49
AhmedShahan <shahan.ahmed001@gmail.com>
2025-09-12 17:39:52 +0600

    cchanges in astream

--- 1_Model/ChatModel/astream.py
+++ 1_Model/ChatModel/astream.py
@@ -1,36 +1,38 @@
+import asyncio
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
+from dotenv import load_dotenv
+load_dotenv()
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0.5
+)
+import time
+stime=time.time()
+for chunk in llm.stream("Write a short story about a cat in a futuristic city."):
+    print(chunk.content, end="", flush=True)
+etime=time.time()
+
+print("Total time:", etime - stime)
+###########################  otal time: 17.07764220237732
+
 # import asyncio
-# from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
 # from dotenv import load_dotenv
+
 # load_dotenv()
+
 # llm = ChatGoogleGenerativeAI(
-#     model="gemini-1.5-flash",
-#     temperature=0.5
+#     model="gemini-2.5-flash",
+#     temperature=0.5,
 # )
 
 # async def main():
 #     async for chunk in llm.astream("Write a short story about a robot in space."):
 #         print(chunk.content, end="", flush=True)
 
+# import time
+# stime=time.time()
 # asyncio.run(main())
-
-
-
-import asyncio
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=0.5
-)
-
-async def main():
-    async for chunk in llm.astream("Write a short story about a robot in space."):
-        text = chunk.content or ""
-        for char in text:
-            print(char, end="", flush=True)
-            await asyncio.sleep(0.05)  # adjust speed here
-
-asyncio.run(main())
+# etime=time.time()
+# print("Total time:", etime - stime)
+########### Total time: 13.485547065734863
