7a9a497ecc1e20663707b11ec4e448ff267561cd
AhmedShahan <shahan.ahmed001@gmail.com>
2026-04-20 14:02:28 +0600

    weather appi is added

--- observibility_with_langSmith/8_agent.py
+++ observibility_with_langSmith/8_agent.py
@@ -1,14 +1,34 @@
 from dotenv import load_dotenv
+from langchain_core.tools import tool
+import requests
 load_dotenv()
 
 ## ------------Tools ------------- ##
 ## Tavily Search Tools
-from langchain_community.tools.tavily_search import TavilySearchResults
+# from langchain_community.tools.tavily_search import TavilySearchResults
 
-search_tool = TavilySearchResults(
-    max_results=5,          # Number of results to return
-    search_depth="advanced" # "basic" or "advanced"
-)
+# search_tool = TavilySearchResults(
+#     max_results=5,          # Number of results to return
+#     search_depth="advanced" # "basic" or "advanced"
+# )
 
-result = search_tool.invoke("What is LangChain?")
-print(result)
+# result = search_tool.invoke("What is LangChain?")
+# print(result)
+
+import os
+### Weather API
+def get_weather_data(city: str) -> dict:
+    geo = requests.get(
+        f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
+    ).json()
+
+    lat = geo["results"][0]["latitude"]
+    lon = geo["results"][0]["longitude"]
+
+    weather = requests.get(
+        f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true&timezone=auto"  # 👈 add timezone=auto
+    ).json()
+
+    return weather
+
+print(get_weather_data("Dhaka"))
