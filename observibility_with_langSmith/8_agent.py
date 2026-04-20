217bc42bc01fb122ef8b41bd6c79db2ba31d125e
AhmedShahan <shahan.ahmed001@gmail.com>
2026-04-20 14:11:08 +0600

    arxive tools is added

--- observibility_with_langSmith/8_agent.py
+++ observibility_with_langSmith/8_agent.py
@@ -17,18 +17,41 @@ load_dotenv()
 
 import os
 ### Weather API
-def get_weather_data(city: str) -> dict:
-    geo = requests.get(
-        f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
-    ).json()
+# def get_weather_data(city: str) -> dict:
+#     geo = requests.get(
+#         f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
+#     ).json()
 
-    lat = geo["results"][0]["latitude"]
-    lon = geo["results"][0]["longitude"]
+#     lat = geo["results"][0]["latitude"]
+#     lon = geo["results"][0]["longitude"]
 
-    weather = requests.get(
-        f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true&timezone=auto"  # 👈 add timezone=auto
-    ).json()
+#     weather = requests.get(
+#         f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true&timezone=auto"  # 👈 add timezone=auto
+#     ).json()
 
-    return weather
+#     return weather
 
-print(get_weather_data("Dhaka"))
+# print(get_weather_data("Dhaka"))
+
+
+
+@tool
+def ArxiveTool(query: str, k: int, full_document: bool = False, load_all_meta: bool = False) -> list:
+    """Fetch Document from Arxiv using Arxiv Retriever"""
+    from langchain_community.retrievers import ArxivRetriever
+
+    retriever = ArxivRetriever(
+        top_k_results=k,
+        get_full_documents=full_document,
+        load_all_available_meta=load_all_meta,
+    )
+    docs = retriever.invoke(query)
+    return docs
+
+
+query = "au:Shafin_Rahman"  # 👈 this is the fix
+results=ArxiveTool.invoke({
+    "query": query,
+    "k":3
+})
+print(results)
